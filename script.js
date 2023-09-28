848a006017c0fb02c936e3b9ba764eb5260ccef4
Maxwell Heath <maxheathbusiness@gmail.com>
2023-09-28 12:48:52 -0600

    Server actually works now and will display 3 albums from search but it is not doing so well actually looking up the right artist im putting in the search, clicking on the albums is not finished as well

--- script.js
+++ script.js
@@ -1,29 +1,35 @@
-const apiKey = '281e00c1f28c2b07c3c2d19fe6a6e49d';
+const apiKey = '281e00c1f28c2b07c3c2d19fe6a6e49d';  // For development only, move it to a secure place for production
 
 function search() {
     const query = $('#searchInput').val();
-    console.log()
+    console.log('Search query:', query);
     if (!query) return;
 
     const url = `http://localhost:3000/search?apikey=${apiKey}&q_artist=${query}&page_size=3&page=1&s_release_date=desc`;
 
     fetch(url)
-        .then(response => response.json())
-    
-        .then(data => {
-            console.log(data)
-            return displayAlbums(data.message.body.album_list, query)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok ' + response.statusText);
+            }
+            return response.json();
         })
-       
-        .catch(() => $('#result').html('Error occurred while fetching data'));
+        .then(data => {
+            console.log(data);
+            return displayAlbums(data.message.body.track_list, query);
+        })
+        .catch(error => {
+            console.error(error);
+            $('#result').html('Error occurred while fetching data');
+        });
 }
 
-function displayAlbums(albums, artist) {
-    console.log(artist)
-    console.log(albums)
+function displayAlbums(tracks, artist) {
+    console.log(artist);
+    console.log(tracks);
     let html = '<ul>';
-    albums.forEach(album => {
-        html += `<li class="album" data-album-id="${album.album.album_id}">${album.album.album_name}</li>`;
+    tracks.forEach(track => {
+        html += `<li class="album" data-album-id="${track.track.album_id}">${track.track.album_name}</li>`;
     });
     html += '</ul>';
     $('#albums').html(html);
@@ -38,9 +44,17 @@ function fetchTracks(albumId, artist) {
     const url = `http://localhost:3000/album_tracks?apikey=${apiKey}&album_id=${albumId}`;
 
     fetch(url)
-        .then(response => response.json())
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok ' + response.statusText);
+            }
+            return response.json();
+        })
         .then(data => displayTracks(data.message.body.track_list, artist))
-        .catch(() => $('#result').html('Error occurred while fetching tracks'));
+        .catch(error => {
+            console.error(error);
+            $('#result').html('Error occurred while fetching tracks');
+        });
 }
 
 function displayTracks(tracks, artist) {
